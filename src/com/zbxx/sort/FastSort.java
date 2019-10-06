446c07c1b2c664da78e6ed65cf9e55d62f8f3226
xiaoxiaofans <18296117471@163.com>
2019-10-06 14:01:30 +0800

    insertSort

--- src/com/zbxx/sort/FastSort.java
+++ src/com/zbxx/sort/FastSort.java
@@ -4,9 +4,36 @@ import java.util.Arrays;
 
 public class FastSort {
 
-	public static void fastSort(int arr[], int le, int ri) {
+
+	public static void fastSort(int arr[], int left, int right) {
+		if (left < right) {
+			int le = left;
+			int ri = right;
+			int temp = arr[le];
+			while (le < ri) {
+				while (ri > le && arr[ri] >= temp) {
+					ri--;
+				}
+				if (le < ri) {
+					arr[le++] = arr[ri];
+				}
+				while (le < ri && arr[le] <= temp) {
+					le++;
+				}
+				if (ri > le) {
+					arr[ri--] = arr[le];
+				}
+			}
+			if (le == ri) {
+				arr[le] = temp;
+			}
+			fastSort(arr, left, le - 1);
+			fastSort(arr, le + 1, right);
+		}
+
 
 	}
+
 
 	public static void main(String[] args) {
 		int arr[] = {1, 2, 32, 3, 8, 4, 6};
---
+++ src/com/zbxx/sort/InsertSort.java
@@ -0,0 +1,28 @@
+package src.com.zbxx.sort;
+
+import java.util.Arrays;
+
+public class InsertSort {
+
+
+	public static void insertSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < arr[i - 1]) {
+				int temp = arr[i];
+				int j;
+				//将小的全部往后挪
+				for (j = i - 1; j > 0 && temp < arr[j]; j--) {
+					arr[j + 1] = arr[j];
+				}
+				arr[j + 1] = temp;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 32, 3, 8, 4, 6};
+		insertSort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
