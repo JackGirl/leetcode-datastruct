39262016f186c730c6a11a41152e92c0e2c261db
xiaoxiaofans <18296117471@163.com>
2021-05-19 20:48:08 +0800

    原地归并

--- src/com/zbxx/sort/MergeSort.java
+++ src/com/zbxx/sort/MergeSort.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class MergeSort {
 
 
+    private static int[] aux = new int[11];
+
     /**
      * @description :  左右 两边合并排序 start-end
      * @author :    万仁杰
@@ -12,26 +14,24 @@ public class MergeSort {
      * @modifyDate :
      */
     public static void merge(int arr[], int start, int mid, int end) {
-        int data[] = new int[end - start + 1];
-        int left = start;
-        int right = mid + 1;
-        int index = 0;
-        while (left <= mid && right <= end) {
-            if (arr[left] < arr[right]) {
-                data[index++] = arr[left++];
-                continue;
+        int lo = start;
+        int j = mid + 1;
+        for (int i = lo; i <= end; i++) {
+            aux[i] = arr[i];
+        }
+
+        for (int i = lo; i <= end; i++) {
+            if (j > end) {
+                arr[i] = aux[lo++];
+            } else if (lo > mid) {
+                arr[i] = aux[j++];
+            } else if (aux[lo] > aux[j]) {
+                arr[i] = aux[j++];
+            } else {
+                arr[i] = aux[lo++];
             }
-            data[index++] = arr[right++];
         }
-        while (right <= end) {
-            data[index++] = arr[right++];
-        }
-        while (left <= mid) {
-            data[index++] = arr[left++];
-        }
-        for (int i = 0; i < data.length; i++) {
-            arr[start + i] = data[i];
-        }
+
     }
 
     public static void mergetSort(int[] arr, int left, int end) {
@@ -45,7 +45,7 @@ public class MergeSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 32, 54, 3, 65, 8, 0, 9};
+        int[] arr = {1, 32, 54, 3, 65, 8, 0, 9, 5, 911, 45};
         mergetSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
