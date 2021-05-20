943d4a43b925d350736e5e43b0650cf169f20fad
xiaoxiaofans <18296117471@163.com>
2021-05-20 16:20:10 +0800

    数量相同按下标排序改位单词排序

--- src/com/zbxx/leetcode/topKFrequent.java
+++ src/com/zbxx/leetcode/topKFrequent.java
@@ -10,36 +10,28 @@ public class topKFrequent {
 
 
     public static void main(String[] args) {
-        String[] arr = {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"};
-        System.out.println(Arrays.toString(topKFrequent(arr, 4).toArray()));
+        String[] arr = {"aaa", "a"};
+        System.out.println(Arrays.toString(topKFrequent(arr, 1).toArray()));
     }
 
 
     public static List<String> topKFrequent(String[] words, int k) {
         //记录起始index
-        Map<String, int[]> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < words.length; i++) {
-            int[] t = map.get(words[i]);
-            if (t == null) {
-                int[] s = new int[2];
-                s[0] = i;
-                s[1]++;
-                map.put(words[i], s);
-            } else {
-                t[1]++;
-            }
+            map.merge(words[i], 1, Integer::sum);
         }
         List<String> s = new ArrayList<>();
-        List<Map.Entry<String, int[]>> d = new ArrayList<>(map.entrySet());
-        Collections.sort(d, new Comparator<Map.Entry<String, int[]>>() {
+        List<Map.Entry<String, Integer>> d = new ArrayList<>(map.entrySet());
+        Collections.sort(d, new Comparator<Map.Entry<String, Integer>>() {
             @Override
-            public int compare(Map.Entry<String, int[]> o1, Map.Entry<String, int[]> o2) {
-                int[] a1 = o1.getValue();
-                int[] a2 = o2.getValue();
-                if (a1[1] > a2[1]) {
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                Integer a1 = o1.getValue();
+                Integer a2 = o2.getValue();
+                if (a1 > a2) {
                     return -1;
                 }
-                if (a1[1] == a2[1]) {
+                if (a1.equals(a2)) {
                     return o1.getKey().compareTo(o2.getKey());
                 } else {
                     return 1;
