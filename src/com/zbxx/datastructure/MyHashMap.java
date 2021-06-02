4dbfb5f2b832c78b3d0c93630cd2374ccb3ccc3d
xiaoxiaofans <18296117471@163.com>
2021-06-02 14:02:34 +0800

    添加pom maven管理

--- src/com/zbxx/datastructure/MyHashMap.java
+++ src/com/zbxx/datastructure/MyHashMap.java
@@ -1,41 +1,147 @@
 package com.zbxx.datastructure;
 
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
 /**
- * @author: soft
- * <description>
- * practice map
- * </description>
- * @date: 2020/1/6
+ * hashMao 红黑树
+ *
+ * @param <K>
+ * @param <V>
  */
-public class MyHashMap<K, V> {
+public class MyHashMap<K extends Comparable, V> {
 
+    /**
+     * 数据
+     */
+    Map.Entry<K, V>[] table;
+    /**
+     * 默认大小
+     */
+    final static Integer DEFAULT_CAPACITY = 16;
+    /**
+     * 树节点转链表大小
+     */
+    final static Integer TREE_2_NODE      = 6;
+    /**
+     * 节点转树大小
+     */
+    final static Integer NODE_2_TREE      = 8;
+    /**
+     * elementSize
+     */
+    int size;
 
-    Object[] data;
-
-    private final static int MIN_SIZE = 16;
 
     public MyHashMap() {
-        data = new Object[MIN_SIZE];
+        this(DEFAULT_CAPACITY);
     }
 
-    public MyHashMap(int size) {
-        if (size < MIN_SIZE) {
-            data = new Object[MIN_SIZE];
-        } else {
-            data = new Object[size];
+    public MyHashMap(int initCapacity) {
+        int capacity = initCapacity <= 0 ? DEFAULT_CAPACITY : initCapacity;
+        table = new Map.Entry[initCapacity];
+    }
+
+    private int index(K key) {
+        return (key == null) ? 0 : key.hashCode() % table.length;
+    }
+
+
+    private void resize() {
+    }
+
+
+    public boolean containsKey(K k) {
+        return get(k) != null;
+    }
+
+    public V put(K k, V v) {
+        return v;
+    }
+
+    public V get(K k) {
+        return null;
+    }
+
+    public boolean remove(K k) {
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    @Data
+    static class ListNode<K extends Comparable, V> implements Map.Entry<K, V> {
+
+        K k;
+
+        V v;
+
+        ListNode<K, V> next;
+
+        public ListNode(K k, V v) {
+            this.k = k;
+            this.v = v;
+        }
+
+
+        @Override
+        public K getKey() {
+            return null;
+        }
+
+        @Override
+        public V getValue() {
+            return null;
+        }
+
+        @Override
+        public V setValue(V value) {
+            V old = v;
+            v = value;
+            return old;
         }
     }
 
-    public void put(K k, V v) {
+    @Data
+    static class TreeNode<K extends Comparable, V> implements Map.Entry<K, V> {
 
-        int h;
-        System.out.println((k == null) ? 0 : (h = k.hashCode()) ^ (h >>> 16));
-    }
+        K k;
+
+        V v;
+
+        TreeNode<K, V> parent;
+
+        TreeNode<K, V> left;
+
+        TreeNode<K, V> right;
+
+        @Override
+        public K getKey() {
+            return k;
+        }
+
+        @Override
+        public V getValue() {
+            return v;
+        }
+
+        @Override
+        public V setValue(V value) {
+            V old = v;
+            v = value;
+            return old;
+        }
 
 
-    public static void main(String[] args) {
-        MyHashMap<String, Object> myHashMap = new MyHashMap<>();
-        myHashMap.put("我", "1");
     }
 
 
