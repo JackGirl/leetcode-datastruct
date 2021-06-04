a1e92428a210ef44636f2f73e5a7e426c00ed594
xiaoxiaofans <18296117471@163.com>
2021-06-04 09:44:14 +0800

    leetcode 160 相交链表  找相交点

---
+++ src/com/zbxx/leetcode/struct/ListNode.java
@@ -0,0 +1,14 @@
+package com.zbxx.leetcode.struct;
+
+public class ListNode {
+
+    public int val;
+
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+
+}
